f5398dd093b941fd4ecaca581678302ce44c8775
Carina <carrie.poell@gmail.com>
2019-12-21 14:08:21 +0100

    feat: add database model.

--- harmony-proj/garden/models.py
+++ harmony-proj/garden/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+URGENCY_CHOICES = [
+    ("highest", "high", "medium", "low"),
+]
+
+TYPE_CHOICES = [
+    ("code", "write", "speak", "order"),
+]
+
+class Task(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    due_date = models.DateTimeField('due')
+    urgency = models.CharField(max_length=50, choices=URGENCY_CHOICES)
+    type = models.CharField(max_length=50, choices=TYPE_CHOICES)
